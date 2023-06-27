7514a9738e1b443e2b4feffac86cf9ee6fedf7e6
Modibo <tr.modibo26@gmail.com>
2023-06-27 12:19:07 +0200

    test

--- src/layouts/dashboard/DashboardLayout.jsx
+++ src/layouts/dashboard/DashboardLayout.jsx
@@ -30,7 +30,9 @@ const Main = styled('div')(({ theme }) => ({
 
 export default function DashboardLayout() {
     const [open, setOpen] = useState(false);
+
     const { data } = useFetchUserByUsername();
+    
     console.log(data)
 
     return (
